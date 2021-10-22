a853712873127f13830a2c579ecad63fdc0f8448
Chetan Gupta <chetang12222@gmail.com>
2021-10-23 04:40:42 +0530

    secondchange

--- src/Components/Game/Game.js
+++ src/Components/Game/Game.js
@@ -2,16 +2,36 @@ import './Game.css';
 import Images from './Images';
 import {useState , useEffect } from "react";
 import {shuffle} from 'lodash';
+import Box from '@material-ui/core/Box';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import Modal from '@material-ui/core/Modal';;
 
 
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
 
 function Game() {
   const [cards,setCards] = useState( shuffle([...Images, ...Images]) );
   const [clicks,setClicks] = useState(0);
+  const [best,setBest] = useState(0);
+  const [val,setVal] = useState(0);
   const [won,setWon] = useState(false);
   const [activeCards,setActiveCards] = useState([]);
   const [foundPairs,setFoundPairs] = useState([]);
-
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+  
   useEffect(() => { document.body.style.backgroundColor = '#33ccff' }, []);
 
   useEffect(() => {
@@ -24,12 +44,20 @@ function Game() {
     };
   }, []);
 
+ 
 
   const handleBeforeUnload = (e) => {
     e.preventDefault();
     e.returnValue = '';
   };
   
+  function Restart(){
+    setFoundPairs([]);
+    setActiveCards([]);
+    setWon(false);
+    setClicks(0);
+    setVal(0);
+  }
 
   function flipCard(index) {
     if (won) {
@@ -37,6 +65,7 @@ function Game() {
       setFoundPairs([]);
       setActiveCards([]);
       setWon(false);
+      setVal(val+1);
       setClicks(0);
     }
     if( activeCards.indexOf(index) !== -1 || foundPairs.indexOf(index) !== -1 ){
@@ -52,6 +81,14 @@ function Game() {
       if (cards[firstIndex] === cards[secondsIndex]) {
         if (foundPairs.length + 2 === cards.length) {
           setWon(true);
+          if(val > 1){
+             if(best > clicks){
+                 setBest(clicks);
+             }
+          }
+          else{
+            setBest(clicks);
+           }
         }
         setFoundPairs( [...foundPairs, firstIndex, secondsIndex] );
       }
@@ -65,9 +102,11 @@ function Game() {
   }
 
   return (
-   
    <div>
-     
+     <Box m={2} pt={3}>
+     <center> <Button variant="contained" onClick={() => Restart()} > Restart </Button> &nbsp;
+       
+       <Button variant="contained" onClick={handleOpen}>How to Play</Button> </center></Box>
       <div className="board">
         {cards.map((card,index) => {
           const flippedToFront =  (activeCards.indexOf(index) !== -1) || (foundPairs.indexOf(index) !== -1 );
@@ -84,16 +123,34 @@ function Game() {
           );
         })}
       </div>
+        
       <div className="stats">
         {won && (
-          <>You won the game! Congratulations!<br />
+          <> You won the game! Congratulations!<br />
             Click any card to play again.<br /><br />
           </>
         )}
-        Attempts: {clicks} &nbsp;&nbsp;&nbsp; Score : {foundPairs.length/2}
-       
+        Attempts: {clicks} &nbsp;&nbsp;&nbsp; Score : {foundPairs.length/2}  &nbsp;&nbsp;&nbsp; Best Score: {best}
       </div>
-  </div>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+
+          Start the game by flipping a card. 
+          Then try to find another card that has the same image as the first. 
+          If you can't find a pair, the flipped cards will be flipped back with the face down. 
+          Try to remember these images as it becomes easier to find pairs the longer you play.
+
+          </Typography>
+        </Box>
+      </Modal>
+
+      </div>
   );
 }
 
