1e7979bba24e9c467e4191b6d1995218a10e2120
Chetan Gupta <chetang12222@gmail.com>
2021-10-22 21:21:38 +0530

    first commit

---
+++ src/Images.js
@@ -0,0 +1,12 @@
+import Image01 from './alphabets/01.jpg';
+import Image02 from './alphabets/02.jpg';
+import Image03 from './alphabets/03.jpg';
+import Image04 from './alphabets/04.jpg';
+import Image05 from './alphabets/05.jpg';
+import Image06 from './alphabets/06.jpg';
+import Image07 from './alphabets/07.jpg';
+import Image08 from './alphabets/08.jpg';
+
+
+const Images = [Image01, Image02, Image03, Image04, Image05, Image06, Image07, Image08];
+export default Images;
